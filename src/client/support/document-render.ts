6e786de889f58014942ee42468c0a44cfe9d3c11
noxabellus <noxabellus@gmail.com>
2024-05-04 13:28:43 -0500

    fixed embedding styles

--- src/client/support/document-render.ts
+++ src/client/support/document-render.ts
@@ -8,40 +8,45 @@ export type SectionFn = (sectionIndex: number, section: Section, options: Option
 export type DocumentTemplate = (title: string, theme: FullTheme, content: string) => string;
 export type EmptyTemplate = (sectionIndex: number, style: string, attributes: AttributeMap) => string;
 export type SectionTemplate = (sectionIndex: number, style: string, content: string, attributes: AttributeMap) => string;
-export type EmbedTemplate = (sectionIndex: number, style: string, embed: Embed, attributes: AttributeMap) => string;
 export type AttributeProcessor = (attributes: AttributeMap, options: Options) => ElemTemplate;
+export type EmbedTemplate<T> = (embed: T, attributes: AttributeMap) => string;
 export type ElemTemplate = (content: string, attributes: AttributeMap) => string;
 
 export type Attributes = {
     attributes: AttributeMap,
 }
 
-export type Section
-    = OpSection
-    | EmbedSection
-    ;
-
-export type OpSection = OpSectionContent & Attributes;
-export type EmbedSection = EmbedSectionContent & Attributes;
-
-export type SectionContent
-    = OpSectionContent
-    | EmbedSectionContent
-    ;
-
-export type OpSectionContent = {
+export type Section = {
     ops: Op[],
-};
+    attributes: AttributeMap,
+}
 
-export type EmbedSectionContent
-    = { embed: Embed }
-    ;
+// export type Section
+//     = OpSection
+//     | EmbedSection
+//     ;
 
-export type Embed
-    = ImageEmbed
-    ;
+// export type OpSection = OpSectionContent & Attributes;
+// export type EmbedSection = EmbedSectionContent & Attributes;
 
-export type ImageEmbed= { "image": string };
+// export type SectionContent
+//     = OpSectionContent
+//     | EmbedSectionContent
+//     ;
+
+// export type OpSectionContent = {
+//     ops: Op[],
+// };
+
+// export type EmbedSectionContent
+//     = { embed: Embed }
+//     ;
+
+// export type Embed
+//     = ImageEmbed
+//     ;
+
+// export type ImageEmbed= { "image": string };
 
 export type Format = {
     section: SectionFn,
@@ -62,11 +67,6 @@ export type SectionTemplates = {
     empty: EmptyTemplate,
     line: SectionTemplate,
     header: SectionTemplate,
-    embeds: EmbedTemplates,
-};
-
-export type EmbedTemplates = {
-    image: EmbedTemplate,
 };
 
 export type InlineTemplates = {
@@ -78,13 +78,14 @@ export type InlineTemplates = {
     underline: ElemTemplate,
     strike: ElemTemplate,
     font: ElemTemplate,
+    embeds: EmbedTemplates,
 };
 
+export type EmbedTemplates = {
+    image: EmbedTemplate<ImageEmbed>,
+};
 
-export function isOpSection (section: Section): section is OpSection {
-    return "ops" in section;
-}
-
+export type ImageEmbed = { image: string };
 
 
 export default function convertDocument (doc: Document, format: Format, userOptions?: Partial<Options>): string {
@@ -93,11 +94,16 @@ export default function convertDocument (doc: Document, format: Format, userOpti
         sectionTemplates: {
             ...format.defaultOptions.sectionTemplates,
             ...(userOptions?.sectionTemplates || {}),
-            embeds: { ...format.defaultOptions.sectionTemplates.embeds, ...(userOptions?.sectionTemplates?.embeds || {}) },
         },
-
-        inlineTemplates: { ...format.defaultOptions.inlineTemplates, ...(userOptions?.inlineTemplates || {}) },
-        attributeProcessors: { ...format.defaultOptions.attributeProcessors, ...(userOptions?.attributeProcessors || {}) },
+        inlineTemplates: {
+            ...format.defaultOptions.inlineTemplates,
+            ...(userOptions?.inlineTemplates || {}),
+            embeds: { ...format.defaultOptions.inlineTemplates.embeds, ...(userOptions?.inlineTemplates?.embeds || {}) },
+        },
+        attributeProcessors: {
+            ...format.defaultOptions.attributeProcessors,
+            ...(userOptions?.attributeProcessors || {}),
+        },
         postProcess: userOptions?.postProcess || format.defaultOptions.postProcess,
     };
 
@@ -117,45 +123,12 @@ export default function convertDocument (doc: Document, format: Format, userOpti
             // handle section endings
 			currentSection.attributes = op.attributes || {};
 			newOpSection();
-		} else if (typeof op.insert === "object") {
-            let embedSection: EmbedSection;
-
-            const keys = Object.keys(op.insert);
-            if (keys.length !== 1) {
-                console.warn("unexpected embed object", op);
-                throw "unexpected embed object";
-            }
-
-            switch (keys[0]) {
-                case "image":
-                    if (typeof op.insert.image !== "string") {
-                        console.warn("unexpected image object content", op.insert.image);
-                        throw "unexpected image object content";
-                    }
-                    embedSection = {
-                        embed: op.insert as ImageEmbed,
-                        attributes: op.attributes || {},
-                    };
-                break;
-
-                default:
-                    console.warn("unexpected embed object", op);
-                    throw "unexpected embed object";
-            }
-
-            sections.push(embedSection);
-
-            currentSection = embedSection;
 		} else if (typeof op.insert == "string" && op.insert.indexOf("\n") >= 0) {
             // break up interior new lines into separate section objs
 			const chunks = op.insert.split("\n");
 
 			chunks.forEach((chunk: string, index: number): void => {
-                if (!isOpSection(currentSection)) {
-                    newOpSection();
-                }
-
-                (currentSection as OpSection).ops.push({
+                currentSection.ops.push({
                     insert: chunk,
                     attributes: op.attributes,
                 });
@@ -163,12 +136,8 @@ export default function convertDocument (doc: Document, format: Format, userOpti
                 if (chunks.length - 1 !== index) newOpSection();
             });
 		} else {
-            // inline text segment
-            if (!isOpSection(currentSection)) {
-                newOpSection();
-            }
-
-			(currentSection as OpSection).ops.push(op);
+            // inline segment
+			currentSection.ops.push(op);
 		}
 	});
 
@@ -194,21 +163,14 @@ export const HtmlFormat: Format = {
         const styleInner = sectionAttributesToCss(section.attributes);
         const style = styleInner.length > 0 ? ` style="${styleInner}"` : "";
 
-        if (isOpSection(section)) {
-            const content = section.ops.map(op => opToContent(op, options)).join("");
+        const content = section.ops.map(op => opToContent(op, options)).join("");
 
-            if (content === "") {
-                return options.sectionTemplates.empty(sectionIndex, style, section.attributes);
-            } else if (section.attributes.header) {
-                return options.sectionTemplates.header(sectionIndex, style, content, section.attributes);
-            } else {
-                return options.sectionTemplates.line(sectionIndex, style, content, section.attributes);
-            }
+        if (content === "") {
+            return options.sectionTemplates.empty(sectionIndex, style, section.attributes);
+        } else if (section.attributes.header) {
+            return options.sectionTemplates.header(sectionIndex, style, content, section.attributes);
         } else {
-            const embedType = Object.keys(section.embed)[0] as keyof EmbedTemplates;
-            const embedTemplate = options.sectionTemplates.embeds[embedType];
-
-            return embedTemplate(sectionIndex, style, section.embed, section.attributes);
+            return options.sectionTemplates.line(sectionIndex, style, content, section.attributes);
         }
     },
 
@@ -262,29 +224,6 @@ export const HtmlFormat: Format = {
             },
 
             header: (_sectionNumber, style, content, attrs) => `<h${attrs.header}${style}>${content}</h${attrs.header}>`,
-
-            embeds: {
-                image: (_sectionNumber, _style, embed, attrs) => {
-                    let size = "";
-                    if (attrs.width) size += ` width=${attrs.width}`;
-                    if (attrs.height) size += ` height=${attrs.height}`;
-
-                    let style = "";
-                    switch (attrs.align) {
-                        case "center":
-                            style += " display: block; margin-left: auto; margin-right: auto;";
-                        break;
-                        case "right":
-                            style += " float: right;";
-                        break;
-                        case "left":
-                            style += " float: left;";
-                        break;
-                    }
-                    if (style.length > 0) style = ` style="${style}"`;
-                    return `<img src="${embed.image}" alt="${attrs.alt}"${size}${style}/>`;
-                },
-            }
         },
 
         inlineTemplates: {
@@ -295,7 +234,17 @@ export const HtmlFormat: Format = {
             italic: content => `<i>${content}</i>`,
             underline: content => `<u>${content}</u>`,
             strike: content => `<s>${content}</s>`,
-            font: (content, attrs) => `<span style="font-family:${attrs.font}">${content}</span>`
+            font: (content, attrs) => `<span style="font-family:${attrs.font}">${content}</span>`,
+
+            embeds: {
+                image: (embed, attrs) => {
+                    let size = "";
+                    if (attrs.width) size += ` width=${attrs.width}`;
+                    if (attrs.height) size += ` height=${attrs.height}`;
+
+                    return `<img src="${embed.image}" alt="${attrs.alt}"${size}/>`;
+                },
+            }
         },
 
         attributeProcessors: {},
@@ -307,8 +256,7 @@ export const HtmlFormat: Format = {
 
 function opToContent (op: Op, options: Options): string {
     if (typeof op.insert !== "string") {
-        console.warn("unexpected op configuration", op);
-        throw "unexpected op configuration";
+        return embedHtml(op.insert, op.attributes || {}, options);
     }
 
     if (!op.attributes) {
@@ -329,6 +277,30 @@ function sectionAttributesToCss (attributes: AttributeMap): string {
                 return "";
         }
     }).join("");
+}
+
+function embedHtml (embed: unknown, attributes: AttributeMap, options: Options): string {
+    if (!embed) {
+        console.warn("missing embed", embed, attributes);
+        throw "missing embed";
+    }
+
+    const keys = Object.keys(embed);
+
+    if (keys.length !== 1) {
+        console.warn("invalid embed", embed, attributes);
+        throw "invalid embed";
+    }
+
+    const embedType = Object.keys(embed)[0];
+    const embedTemplate = (options.inlineTemplates.embeds as any)[embedType];
+
+    if (!embedTemplate) {
+        console.warn("missing embed template", embed, attributes);
+        throw "missing embed template";
+    }
+
+    return embedTemplate(embed, attributes);
 }
 
 function innerHtml (content: string, attrs: AttributeMap, options: Options): string {
